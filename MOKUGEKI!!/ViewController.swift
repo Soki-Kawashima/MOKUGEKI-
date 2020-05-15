aff4f33ea5f1bf529ae4606cc84c8421592beb99
Soki Kawashima <mokorokokoroko@gmail.com>
2020-05-15 15:10:16 +0900

    Initial Commit

---
+++ MOKUGEKI!!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOKUGEKI!!
+//
+//  Created by 川島壮生 on 2020/05/15.
+//  Copyright © 2020 川島壮生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
